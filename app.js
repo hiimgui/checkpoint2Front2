7c497d7018bb7b6c0c0a04b6e3c3b4ef2e64c7e4
Guilhermekageyama <guilhermekageyama@pop-os.localdomain>
2021-09-22 18:16:02 -0300

    tentativa de fazer o botao completar  funcionar

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const listaTodos = document.querySelector(".listaTodos");
 const todos = [];
 let date = new Date()
 let today = date.getDate();
-let month = date.getMonth();
+let month = date.getMonth() + 1;
 let year = date.getFullYear();
 initalDate.value = `${today} / ${month} / ${year}`
 
@@ -19,19 +19,39 @@ btnSubmit.addEventListener("click", (e) => {
     e.preventDefault();
 
     //TODO: validação dos inputs, length != 0, descricao.length > 10, if error alert("erro"), opcional data limite
-    if(limitDate.value == null || descricaoTodo.value.length < 10) return alert("error");
+    if(limitDate.value === "" || descricaoTodo.value.length < 10) return alert("error");
     let limitDateValor = limitDate.value.split("-");
     limitDateValor = limitDateValor.reverse();
     limitDateValor = limitDateValor.toString().replace("," , "/");
     limitDateValor = limitDateValor.replace("," , "/");
-    listaTodos.innerHTML += `<div class="card">${descricaoTodo.value} ${limitDateValor} </div>`;
 
-    //TODO: setdata to localstorage aqui, todo todo precisa de um botao delete
+    //Criar um construtor? que vai ser setado no local storage?
+    let card = `<li class="card">${descricaoTodo.value} ${limitDateValor} <button class="terminar">Terminada</button> <button onclick="excluir()">Excluir</button></li>`;
+    todos.push(card);
+    listaTodos.innerHTML += card;
 
-    //TODO checkbox -> strikethrough
+    let terminar = document.querySelector(".terminar");
+    terminar.addEventListener("click", (e)=>{
+    e.preventDefault();
+    console.log("clicou")
+    terminar.parentElement.style.setProperty("text-decoration","line-through");
+    });
+  
+    //TODO checkbox -> strikethrough var result = str.strike();
 
     //esvaziando inputs
     limitDate.value = "";
     descricaoTodo.value = "";
 })
+
+// function terminar(){
+//     console.log("terminada");
+//     descricaoTodo.value.strike();
+// }
+
+
+
+function excluir(){
+    console.log("excluida")
+}
 

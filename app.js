6efd8fb140a5a54c865dcba5afccba8d902fff06
Guilhermekageyama <guilhermekageyama@pop-os.localdomain>
2021-09-24 10:58:47 -0300

    carregar do localstorage, excluir funfando

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const limitDate = document.querySelector(".limitDate");
 const descricaoTodo = document.querySelector(".descricaoTodo")
 const btnSubmit = document.querySelector(".btnSubmit");
 const listaTodos = document.querySelector(".listaTodos");
-const todos = [];
+let todos = [];
+let keyLocalStorage = 'info';
 
 
 let date = new Date()
@@ -14,48 +15,33 @@ let month = date.getMonth() + 1;
 let year = date.getFullYear();
 initalDate.value = `${today} / ${month} / ${year}`
 
-//TODO: onload = getitems do local storage
-let keyLocalStorage = 'info';
-document.addEventListener("load", ()=>{
+//TODO: onload = getitems do local storage //nao ta indo socorro luciano
+window.onload=()=>{
+    console.log("carregando...")
     carregarInfoLocalStorage();
-})
+}
 btnSubmit.addEventListener("click", (e) => {
     e.preventDefault();
-
-    
-    
-    //TODO: validação dos inputs, length != 0, descricao.length > 10, if error alert("erro"), opcional data limite
     if(limitDate.value === "" || descricaoTodo.value.length < 10) return alert("error");
     let limitDateValor = limitDate.value.split("-");
     limitDateValor = limitDateValor.reverse();
     limitDateValor = limitDateValor.toString().replace("," , "/");
     limitDateValor = limitDateValor.replace("," , "/");
 
-    //Criar um construtor? que vai ser setado no local storage?
     let tarefa = {
         descricao: descricaoTodo.value,
         dataLimite:limitDateValor
     }
+    todos.push(tarefa);
     let card = `<li class="card">${descricaoTodo.value} ${limitDateValor} <button class="terminar">Terminada</button> <button onclick="excluir()">Excluir</button></li>`;
-    todos.push(card);
     listaTodos.innerHTML += card;
 
-    // let terminar = document.querySelector(".terminar");
-    // terminar.addEventListener("click", (e)=>{
-    // e.preventDefault();
-    // console.log("clicou")
-    // terminar.parentElement.style.setProperty("text-decoration","line-through");
 
     adicionarLocalStorage(tarefa)
     //esvaziando inputs
     limitDate.value = "";
     descricaoTodo.value = "";
 })
-
-// function terminar(){
-//     console.log("terminada");
-//     descricaoTodo.value.strike();
-// }
 
 let adicionarLocalStorage = (obj) => {
     let infoLocalStorage = localStorage.getItem(keyLocalStorage);
@@ -67,24 +53,21 @@ let adicionarLocalStorage = (obj) => {
 }
 
 let carregarInfoLocalStorage = () => {
+    let infoLocalStorage = localStorage.getItem(keyLocalStorage);
     if(infoLocalStorage !== null){
         todos = JSON.parse(infoLocalStorage)
+        console.log(todos)
     }
-    listaTodos.innerHTML = "";
-    todos.forEach((x,y)=> {
-        let card = `<li class="card">${info.descricao} ${info.dataLimite} <button class="terminar">Terminada</button> <button>Excluir</button></li>`;
-        let terminar = document.querySelector(".terminar").addEventListener("click", (e)=>{
-            e.preventDefault();
-            console.log("clicou")
-            terminar.parentElement.style.setProperty("text-decoration","line-through");
+    let dataToUse =  todos.map((x)=>{
+            return `<li class="card">${x.descricao}</li>`
+            
         })
-        let excluir = document.querySelector(".excluir").addEventListener("click", ()=>{excluir(y)});
-       
-    })
+        listaTodos.innerHTML+=dataToUse    
 }
 
 function excluir(int){
-
+    console.log("clicou excluir")
+    let infoLocalStorage = localStorage.getItem(keyLocalStorage);
     todos = JSON.parse(infoLocalStorage)
     todos.splice(int, 1);
     localStorage.setItem(keyLocalStorage, JSON.stringify(todos));
@@ -93,4 +76,7 @@ function excluir(int){
 }
     
 
-
+function terminar(int){
+    console.log("clicou terminar")
+    terminar.parentElement.style.setProperty("text-decoration","line-through");
+}
